0576b871c99f452d285e4f7e8148dc29ae228c6b
GaboDev <118872317+gaboMC@users.noreply.github.com>
2026-09-12 00:28:25 -0400

    test: caso culpable para probar el quality gate

--- vite.config.js
+++ vite.config.js
@@ -29,24 +29,24 @@ import { configDefaults } from 'vitest/config' // 👈 Importante para no perder
 export default defineConfig({
   plugins: [react()],
   test: {
-    // globals: true,
-    // environment: 'happy-dom',
-    // setupFiles: ['./src/tests/setup.tsx'],
-    // exclude: [
-    //   ...configDefaults.exclude, // Mantiene las carpetas ignoradas por defecto (como node_modules)
-    //   '**/e2e/**',               // 👈 Ignora tu carpeta de Playwright E2E
-    //   '**/tests/**',             // 👈 Ignora carpetas de tests alternativas de Playwright
-    //   '**/*.spec.{ts,tsx}'       // 👈 Ignora cualquier archivo .spec de Playwright
-    // ],
-    // coverage: {
-    //   provider: 'v8',
-    //   reporter: ['text', 'html'],
-    //   thresholds: {
-    //     lines: 25,
-    //     functions: 30,
-    //     branches: 20,
-    //     statements: 25,
-    //   },
-    // },
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/tests/setup.tsx'],
+    exclude: [
+      ...configDefaults.exclude, // Mantiene las carpetas ignoradas por defecto (como node_modules)
+      '**/e2e/**',               // 👈 Ignora tu carpeta de Playwright E2E
+      '**/tests/**',             // 👈 Ignora carpetas de tests alternativas de Playwright
+      '**/*.spec.{ts,tsx}'       // 👈 Ignora cualquier archivo .spec de Playwright
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 25,
+        functions: 30,
+        branches: 20,
+        statements: 25,
+      },
+    },
   },
 })
